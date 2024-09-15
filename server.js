142ef7dcb7e1b1dcaacf9e2eda836144d605722c
elkhaddir <34580174+habladi@users.noreply.github.com>
2024-09-15 13:57:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const http = require('http');
-const hostname = '127.0.0.1';
+const express = require('express');
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('Node.js is working.');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Node.js is working with Express and http.');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// Create an HTTP server using the Express app
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running`);
 });
